dc482c6c442ef9a41701b15de0009adf11ac3fee
Stela <you@example.com>
2020-08-25 22:04:30 -0300

    exe3 da lista 2 - requisito 1 OK

--- lista 2/exe3.js
+++ lista 2/exe3.js
@@ -29,7 +29,15 @@ function chefe(){
 
 }
 function requisito1(vet){ // vet aponta pra vetor
-
+    // inserir habitante
+    var objeto = new Object()
+    objeto.sexo = prompt("Informe M ou F").toUpperCase
+    objeto.altura = Number(prompt("Informe a altura"))
+    objeto.idade = Number(prompt("Informe a idade"))
+    objeto.olhos = prompt("Informe a cor dos olhos A- azul V- verde C- castanho")
+    //insere no vetor
+    vet.push(objeto) // ta inserindo no vetor (escopo global)
+    console.log("Habitante cadastrado com sucesso") 
 }
 function requisito2(vet){
     
